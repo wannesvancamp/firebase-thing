49f315e00c765db4cffbccb0b460b1876dcfbfbf
Wannes Van Camp <wannes.van.camp@student.howest.be>
2022-05-27 12:46:38 +0200

    added stuff probs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-app.js';
 import { getDatabase, ref, child, get, set } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-database.js";
 
+
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDxx2DeNb6hcsxvAloCEdts-8pJGaWf_eI",
@@ -18,23 +19,16 @@ const app = initializeApp(firebaseConfig);
 // Get a reference to the database service
 const database = getDatabase(app);
 
-
-function writeUserData(userId, name, email, imageUrl) {
+function writeData(value) {
     const db = getDatabase();
-    set(ref(db, 'users/' + userId), {
-        username: name,
-        email: email,
-        profile_picture: imageUrl
+    set(ref(db, 'mouse'), {
+        active: value
     });
 }
 
-writeUserData(2, "wannes", "wannes.van.camp@student.howest.be", "picture.jpg")
-    //THIS IS TO WRITE DATA TO DB
 
-
-// userId needs to be replaced with number for some reason
 const dbRef = ref(getDatabase());
-get(child(dbRef, `users/${2}`)).then((snapshot) => {
+get(child(dbRef, `mouse`)).then((snapshot) => {
     if (snapshot.exists()) {
         console.log(snapshot.val());
     } else {
@@ -43,3 +37,19 @@ get(child(dbRef, `users/${2}`)).then((snapshot) => {
 }).catch((error) => {
     console.error(error);
 });
+
+
+
+
+writeData(false)
+
+
+document.querySelector("button").addEventListener("click", function() {
+    if (this.getAttribute("data-value") == "false") {
+        this.setAttribute("data-value", "true")
+    } else {
+        this.setAttribute("data-value", "false")
+    }
+    console.log(document.querySelector("button").getAttribute("data-value"))
+    writeData(this.getAttribute('data-value'))
+})
